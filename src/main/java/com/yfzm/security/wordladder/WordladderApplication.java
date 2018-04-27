92fee8cac5ee28704ba9cc14965ac0e3edbd33e2
Li Hao <yfzm1223@sjtu.edu.cn>
2018-04-27 22:26:41 +0800

    init

---
+++ src/main/java/com/yfzm/security/wordladder/WordladderApplication.java
@@ -0,0 +1,12 @@
+package com.yfzm.security.wordladder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordladderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordladderApplication.class, args);
+    }
+}
